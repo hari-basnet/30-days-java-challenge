ebe0b9988cee4bd1ba84db044217f33823904b69
haribasnet <harikrishnabsnt@gmail.com>
2019-12-01 23:14:19 +0200

    car class more

--- day-one/src/car/Car.java
+++ day-one/src/car/Car.java
@@ -14,15 +14,29 @@ public class Car {
 
     double maxFuel = 16;
     double currentFuel = 8;
+    double mpg = 26.4;
 
+    int numberOfPeopleInCar = 1;
+
+    // f(x,y,z) = x +1 / z -y;
+    // say x = 5;
+    // f(5) = 5 + 1 = 6
+
+    // creating a constructor
+    public Car(int customMaxSpeed, double customWeight, boolean customIsTheCarOn){
+        maxSpeed = customMaxSpeed;
+        weight = customWeight;
+        isTheCarOn = customIsTheCarOn;
+    }
 
     public void printVariables() {
-        System.out.println(maxSpeed);
         System.out.println(minSpeed);
+        System.out.println(maxSpeed);
         System.out.println(weight);
         System.out.println(isTheCarOn);
         System.out.println(condition);
         System.out.println(nameOfCar);
+        System.out.println(numberOfPeopleInCar);
     }
 
     public void wreckCar() {
@@ -34,15 +48,40 @@ public class Car {
         maxSpeed = maxSpeed + 10;
     }
 
+    public void getIn(){
+        numberOfPeopleInCar++;
+    }
+
+    public void getOut(){
+        numberOfPeopleInCar--;
+    }
+
+    public double howManyMilesTillOutOfGas(){
+        return currentFuel * mpg;
+    }
+
+    public double maxMilesPerFillUp(){
+        return maxFuel * mpg;
+    }
+
     public static void main(String[] args) {
 
         // write your code here
-        Car familyCar = new Car();
-        familyCar.printVariables();
-        System.out.println("----------------");
+        System.out.println("birthday car _______");
+        Car birthdayPresent = new Car(500, 5000.55, true );
+        birthdayPresent.printVariables();
+        birthdayPresent.getIn();
+        birthdayPresent.getIn();
 
-        familyCar.upgradeMinSpeed();
-        familyCar.printVariables();
+        System.out.println("Miles left: " + birthdayPresent.howManyMilesTillOutOfGas());
+        System.out.println("Max miles per fillup: " + birthdayPresent.maxMilesPerFillUp());
+
+//        System.out.println("Christmas car ______");
+//        Car christmasPresent = new Car(550, 2000, false);
+//        christmasPresent.printVariables();
+//        christmasPresent.getIn();
+//        christmasPresent.printVariables();
+
 
 
 
