a188784b8632c61140f32117a9e0838f68b661a7
Hari Basnet <hari.basnet@x-akseli.fi>
2020-09-15 13:33:55 +0300

    Creating a custom Queue

--- Queuey/src/com/java/Main.java
+++ Queuey/src/com/java/Main.java
@@ -1,8 +1,25 @@
 package com.java;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Queuey q2 = new Queuey();
+
+        q2.enqueue(2);
+        q2.enqueue(3);
+        q2.enqueue(4);
+        q2.enqueue(5);
+        q2.enqueue(6);
+
+        System.out.println(q2.dequeue());
+        System.out.println(q2.peek());
+
+        System.out.println("Hello there");
+
+
     }
 }
--- Queuey/src/com/java/Queuey.java
+++ Queuey/src/com/java/Queuey.java
@@ -1,7 +1,34 @@
 package com.java;
 
+import java.util.LinkedList;
+
 public class Queuey {
 
+	 private LinkedList queue;
 
-	
+	public Queuey () {
+		queue = new LinkedList();
+	}
+
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
+
+	public int size() {
+		return  queue.size();
+	}
+
+	// enqueing an item
+	public void enqueue(int n) {
+		queue.addLast(n);
+	}
+
+	public int dequeue() {
+		return (int) queue.remove(0);
+	}
+
+	// returns the first item
+	public int peek() {
+		return (int) queue.get(0);
+	}
 }
